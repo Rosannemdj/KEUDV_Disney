d6eeae6d8f081a4e5161a3822677bc5ea66322f9
Rosannemdj <ro.m.de.jong@st.hanze.nl>
2025-03-12 09:12:09 +0100

    omzetten van html naar vue framework

--- Disneycharacters/src/App.vue
+++ Disneycharacters/src/App.vue
@@ -1,47 +1,70 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import characterKart from './components/characterKart.vue';
+
+import mario_png from './assets/images/mario.png';
+import luigi_webp from './assets/images/luigi.webp';
+import toad_png from './assets/images/toad.png';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div id="container">
+      <template v-for="character in characters" :key="character.character">
+        <characterKart :backgroundcolor="character.backgroundcolor" :character="character.character"
+          :image="character.image" />
+      </template>
+    </div>
+    <h3>highlight</h3>
+    <characterKart :backgroundcolor="characters[currentCharacter].backgroundcolor"
+      :character="characters[currentCharacter].character" :image="characters[currentCharacter].image" />
+    <button @click=previousCharacter() >Vorig</button>
+    <button @click=nextCharacter() >Volgende</button>
   </main>
 </template>
+<script>
+
+export default {
+  data() {
+    return {
+      characters: [
+        { backgroundcolor: "red", character: "Mario", image: mario_png },
+        { backgroundcolor: "green", character: "Luigi", image: luigi_webp },
+        { backgroundcolor: "rgb(226, 187, 70)", character: "Toad", image: toad_png }
+      ],
+      currentCharacter: 0
+    }
+  },
+  onMounted() {
+    console.log('onMounted');
+  },
+  methods : {
+    nextCharacter(){
+      this.currentCharacter++;
+      if (this.currentCharacter >= this.characters.length) {
+        this.currentCharacter = 0;
+      }
+    },
+    previousCharacter(){
+      console.log('previousCharacter');	
+      this.currentCharacter--;
+      if (this.currentCharacter < 0) {
+        this.currentCharacter = this.characters.length - 1;
+      }
+    }
+  }
+}
+
+</script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 15px;
 }
 </style>
---
+++ Disneycharacters/src/components/characterKart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="character-container" >
+    <div class="character" :style="{ backgroundColor: backgroundcolor }">
+      <img :src=image alt="Mario" class="plaatje" />
+      <h2>{{ character }}</h2>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'characterKart',
+  props: {
+    backgroundcolor: String,
+    character: String,
+    image: String,
+  }
+}
+</script>
+
+<style scoped>
+
+.plaatje {
+  width: 100px;
+  position: absolute;
+
+}
+
+
+
+.character-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 15px;
+  flex-wrap: nowrap;
+}
+
+.character {
+
+  /* Standaard achtergrondkleur, overschrijf dit per karakter */
+  text-align: center;
+  height: 100px;
+  width: 150px;
+  border-radius: 10%;
+  padding-top: 90px;
+  color: white;
+  font-weight: bold;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  position: relative;
+  margin: 0 7.5px;
+  transition: order 1s ease-in-out;
+}
+</style>
